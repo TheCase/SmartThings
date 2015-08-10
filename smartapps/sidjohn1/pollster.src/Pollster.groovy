cc3b930c79b20bb0cf1c570f655ba2a4962b82f5
Bryan Casey <bryan@caseyclan.net>
2015-08-10 15:31:12 -0600

    namespace

--- smartapps/sidjohn1/pollster.src/Pollster.groovy
+++ smartapps/sidjohn1/pollster.src/Pollster.groovy
@@ -32,10 +32,10 @@
 
 definition(
     name: "Pollster",
-    namespace: "statusbits",
+    namespace: "sidjohn1",
     author: "geko@statusbits.com",
     description: "Poll or refresh device status periodically.",
-    category: "Convenience",
+    category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
