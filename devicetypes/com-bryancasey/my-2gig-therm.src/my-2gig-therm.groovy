a87b9b491e67af46c4fd76de2ad6a36fa328c4c5
Bryan Casey <smartthings@caseyclan.net>
2015-08-10 13:24:47 -0700

    init3

---
+++ devicetypes/com-bryancasey/my-2gig-therm.src/my-2gig-therm.groovy
@@ -0,0 +1,98 @@
+/**
+ *  my 2gig therm
+ *
+ *  Author: Bryan Casey
+ *  Date: 2014-04-07
+ */
+ // for the UI
+metadata {
+	definition (name: "my 2gig therm", namespace: "com.bryancasey", author: "Bryan Casey") {
+		capability "Thermostat"
+		capability "Relative Humidity Measurement"
+	}
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'temperature' attribute
+	// TODO: handle 'heatingSetpoint' attribute
+	// TODO: handle 'coolingSetpoint' attribute
+	// TODO: handle 'thermostatSetpoint' attribute
+	// TODO: handle 'thermostatMode' attribute
+	// TODO: handle 'thermostatFanMode' attribute
+	// TODO: handle 'thermostatOperatingState' attribute
+	// TODO: handle 'humidity' attribute
+
+}
+
+// handle commands
+def setHeatingSetpoint() {
+	log.debug "Executing 'setHeatingSetpoint'"
+	// TODO: handle 'setHeatingSetpoint' command
+}
+
+def setCoolingSetpoint() {
+	log.debug "Executing 'setCoolingSetpoint'"
+	// TODO: handle 'setCoolingSetpoint' command
+}
+
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def heat() {
+	log.debug "Executing 'heat'"
+	// TODO: handle 'heat' command
+}
+
+def emergencyHeat() {
+	log.debug "Executing 'emergencyHeat'"
+	// TODO: handle 'emergencyHeat' command
+}
+
+def cool() {
+	log.debug "Executing 'cool'"
+	// TODO: handle 'cool' command
+}
+
+def setThermostatMode() {
+	log.debug "Executing 'setThermostatMode'"
+	// TODO: handle 'setThermostatMode' command
+}
+
+def fanOn() {
+	log.debug "Executing 'fanOn'"
+	// TODO: handle 'fanOn' command
+}
+
+def fanAuto() {
+	log.debug "Executing 'fanAuto'"
+	// TODO: handle 'fanAuto' command
+}
+
+def fanCirculate() {
+	log.debug "Executing 'fanCirculate'"
+	// TODO: handle 'fanCirculate' command
+}
+
+def setThermostatFanMode() {
+	log.debug "Executing 'setThermostatFanMode'"
+	// TODO: handle 'setThermostatFanMode' command
+}
+
+def auto() {
+	log.debug "Executing 'auto'"
+	// TODO: handle 'auto' command
+}
+
+
