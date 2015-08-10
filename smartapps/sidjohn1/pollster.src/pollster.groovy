2990a0220cc69f72b26141a25cfdaf1eb87435a3
Bryan Casey <bryan@caseyclan.net>
2015-08-10 14:49:00 -0600

    move

--- smartapps/sidjohn1/pollster.src/pollster.groovy
+++ smartapps/sidjohn1/pollster.src/pollster.groovy
@@ -35,7 +35,7 @@ definition(
     namespace: "sidjohn1",
     author: "geko@statusbits.com",
     description: "Poll or refresh device status periodically.",
-    category: "Convenience",
+    category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
