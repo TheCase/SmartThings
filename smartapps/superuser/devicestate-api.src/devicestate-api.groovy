fe2b62bcb38b08629826af1f98c2e0c1983f252d
Bryan Casey <github@repulsor.net>
2018-02-18 14:22:37 -0700

    add status

--- smartapps/superuser/devicestate-api.src/devicestate-api.groovy
+++ smartapps/superuser/devicestate-api.src/devicestate-api.groovy
@@ -21,26 +21,31 @@ preferences {
         input "humidity", "capability.relativeHumidityMeasurement", title: "Which humidity sensor?", multiple: true
         input "power", "capability.powerMeter", title: "Which Power Meters?", multiple: true
         input "battery", "capability.battery", title: "Which Battery Powered Devices?", multiple: true
+	input "status", "capability.status", title: "Which Status Reporters?", multiple: true
 	}
 }
 
 mappings {
 	path("/power") { 		action: [ GET: "listPower" ] }
-	path("/temperature") { 	action: [ GET: "listTemperature" ] }
-	path("/humidity") { 	action: [ GET: "listHumidity" ] }   
-	path("/battery") { 		action: [ GET: "listBattery" ] }   
+	path("/temperature") { 		action: [ GET: "listTemperature" ] }
+	path("/humidity") { 		action: [ GET: "listHumidity" ] }   
+	path("/battery") { 		action: [ GET: "listBattery" ] }  
+	path("/status") { 		action: [ GET: "listStatus" ] }   
+
 }
 
-def listPower() { 		power.collect{device(it,"powerMeter")} }
+def listPower() { 	power.collect{device(it,"powerMeter")} }
 def listTemperature() { temperature.collect{device(it,"temperatureMeasurement")} }
 def listHumidity() { 	humidity.collect{device(it,"relativeHumidityMeasurement")} }
 def listBattery() { 	battery.collect{device(it,"battery")} }
+def listStatus() { 	status.collect{device(it,"status")} }
 
 private device(it, type) {
 	def map = [ temperatureMeasurement: "temperature", 
-    			relativeHumidityMeasurement: "humidity",
-                powerMeter: "power",
-                battery: "battery"
+    		    relativeHumidityMeasurement: "humidity",
+                    powerMeter: "power",
+                    battery: "battery",
+		    status: "status"
                 ] 
 	def attrib = map[type]
     log.debug "${it.label} ${it.currentValue(attrib)}"
